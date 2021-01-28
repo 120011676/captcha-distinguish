573f233d45e37b9d2798d66aeb3ff6514b855e2e
Say.li <120011676@qq.com>
2021-01-28 23:49:41 +0800

    初步可识别验证码测试

--- src/test/java/com/github/qq120011676/captcha/distinguish/CodeTests.java
+++ src/test/java/com/github/qq120011676/captcha/distinguish/CodeTests.java
@@ -3,8 +3,12 @@ package com.github.qq120011676.captcha.distinguish;
 import net.sourceforge.tess4j.ITesseract;
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
+import net.sourceforge.tess4j.util.ImageHelper;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class CodeTests {
@@ -13,14 +17,18 @@ public class CodeTests {
         filenames.add("52JW.png");
         filenames.add("D11X.png");
         filenames.add("DUYA.png");
-        String path = "/Users/say/Downloads/";
+        filenames.add("7364.png");
+        String path = "/home/say/Downloads/";
         ITesseract instance = new Tesseract();
+        instance.setDatapath("/home/say/Downloads/tessdata");
         filenames.forEach(o -> {
             try {
-                String result = instance.doOCR(new File(path + o));
+                BufferedImage bufferedImage = ImageIO.read(new File(path, o));
+                bufferedImage = ImageHelper.convertImageToGrayscale(bufferedImage);
+                String result = instance.doOCR(bufferedImage).trim();
                 String code = o.substring(0, o.indexOf("."));
-                System.out.println(code.equals(result) + "=" + result + ":" + o);
-            } catch (TesseractException e) {
+                System.out.println(code.equals(result) + "=" + result + ":" + code);
+            } catch (TesseractException | IOException e) {
                 e.printStackTrace();
             }
         });
