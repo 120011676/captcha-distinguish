15b74c53763715fd2b7024f23044c505533a119d
Say.li <120011676@qq.com>
2021-01-28 10:58:29 +0800

    init

---
+++ src/main/java/com/github/qq120011676/captcha/distinguish/CaptchaDistinguishApplication.java
@@ -0,0 +1,13 @@
+package com.github.qq120011676.captcha.distinguish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaptchaDistinguishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaptchaDistinguishApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/qq120011676/captcha/distinguish/CaptchaDistinguishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.qq120011676.captcha.distinguish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaptchaDistinguishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
